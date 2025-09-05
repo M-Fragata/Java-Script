b7a3b8c5c5409678bb85c7f60d7ce1dedfc51420
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2025-09-05 11:42:32 -0300

    att

---
+++ mod04/ex001.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+
+if (vel <= 60){
+    console.log(`Você está dentro do limite de velocidade da rodovia`)
+}else{
+    console.log(`Cuidado! Você está acima do limite de velocidade da rodovia`)
+}
