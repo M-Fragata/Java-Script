c89adfccf7b448bb787cf9864aa7706ff90e50b2
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2025-10-30 18:59:01 -0300

    destructurin, rest parameters

---
+++ rocketseat/js-intermediario/jsmodern.js/javascript.js
@@ -0,0 +1,102 @@
+/*
+//O strict mode (modo estrito): ativando esse modo, os erros que eram silenciosos passam a gerar exceções no javascript.
+
+"use strict"
+
+function showMessage() {
+
+    let personName = "Matheus Fragata"
+
+    console.log("Olá", personName)
+}
+
+showMessage()
+
+class Student {
+    get point() {
+        return 7
+    }
+}
+
+let student = new Student()
+// Tenta alterar uma propriedade somente leitura. 
+//student.point = 10
+
+//Quando parametros duplicados, o strict mode acusa erro
+function sum(a, a, c) {
+    return a + a + c
+}
+const result = sum(1 , 3 , 2) //3 + 3 + 2 = 8
+*/
+
+/*
+//                     Aula 02
+//Destructuring Assigment (Desestruturação) permite extrair dados de arrays ou objetos em variáves distintas
+
+const data = [
+    "Matheus Fragata",
+    "matheusdemoraes2@gmail.com"
+]
+
+//Desestruturando array data
+
+const [username, email] = data
+console.log(username)
+console.log(email)
+
+const fruits = [
+    "Banana",
+    "Apple",
+    "Orange",
+]
+
+//Desestruturando somente o primeiro no array fruits
+const [banana] = fruits
+console.log(banana)
+
+//Ignorando o primeiro na desestruturação utilizar virgulas
+const [,apple] = fruits
+console.log(apple)
+*/
+
+/*
+//                      Aula 3
+//Destructuring objetos
+
+const product = {
+    description: "Teclado",
+    price: 150,
+}
+
+const { description, price } = product
+console.log(description)
+console.log(price)
+
+function newProduct({description, price}) {
+
+console.log(description)
+console.log(price)
+
+}
+
+newProduct({
+    description: "Mouse",
+    price: 70
+})
+*/
+
+//                  Aula 4
+// Rest params (...) permite representar um número indefinido de argumentos como um array.
+
+function values(...rest) {
+    //Mostra a quantidade de parâmetros
+    console.log(rest.length)
+
+    // Mostrando o conteúdo do array
+    console.log(...rest)
+
+    //Mostra o conteúdo do rest que é um array
+    console.log(rest)
+}
+
+values(1, 2, "c", 4)
