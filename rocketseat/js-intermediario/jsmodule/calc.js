2face3b00d490280fa742a446831551e751ab893
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2025-11-04 16:00:10 -0300

    att

---
+++ rocketseat/js-intermediario/jsmodule/calc.js
@@ -0,0 +1,9 @@
+//export { sum, multiply }
+
+export function sum(a, b) {
+    return a + b
+}
+
+export function multiply (a, b) {
+    return a * b
+}
---
+++ rocketseat/js-intermediario/jsmodule/main.js
@@ -0,0 +1,3 @@
+import { multiply, sum } from "./calc.js"
+
+console.log("4 + 6 =",sum(4,6))
