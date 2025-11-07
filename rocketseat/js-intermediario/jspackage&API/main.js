e3506dd745a7713c61daf4fb393f189b0e3e9cbc
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2025-11-06 21:55:42 -0300

    server e json

--- rocketseat/js-intermediario/jspackage&API/main.js
+++ rocketseat/js-intermediario/jspackage&API/main.js
@@ -1,2 +1,48 @@
-const date = dayjs()
-console.log(date.format("DD/MM/YY - HH:mm"))
+/*
+//Utilizando fetch com then
+
+fetch("http://localhost:3333/products")
+.then((response) => {response.json()
+.then((result) => {console.log(result)})
+})
+*/
+
+// Utilizando fetch com async/await
+async function fetchProducts(params) {
+    const response = await fetch("http://localhost:3333/products")
+    const result = await response.json()
+    console.log(result)
+}
+
+async function fetchProductById(id) {
+    const response = await fetch(`http://localhost:3333/products/${id}`)
+    const result = await response.json()
+    console.log(result)
+}
+
+//fetchProductById(1762476406066)
+
+//fetchProducts()
+
+const productName = document.querySelector('input#name')
+const productPrice = document.querySelector('input#price')
+const form = document.querySelector('form')
+
+addEventListener('submit', async (event) => {
+    event.preventDefault()
+    console.log("Enviando...")
+
+    await fetch("http://localhost:3333/products", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            id: new Date().getTime().toString(),
+            name: productName.value,
+            price: productPrice.value
+        })      
+    })
+
+    await fetchProducts()
+})
